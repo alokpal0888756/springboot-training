3105f1bf462c0dd0bdd4be4f7dddd63e46172abd
alok0888756 <alok0888756@gmail.com>
2024-04-23 05:44:34 -0400

    added circuit breaker dep

--- wallet-service/src/main/java/com/npci/walletservice/FeignCheck.java
+++ wallet-service/src/main/java/com/npci/walletservice/FeignCheck.java
@@ -1,5 +1,6 @@
 package com.npci.walletservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,4 +10,5 @@ public interface FeignCheck {
 
     @GetMapping("/account/{accountNumber}")
     public Account getAccount(@PathVariable("accountNumber") long accountNumber);
+
 }
--- wallet-service/src/main/java/com/npci/walletservice/WalletService.java
+++ wallet-service/src/main/java/com/npci/walletservice/WalletService.java
@@ -1,5 +1,6 @@
 package com.npci.walletservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +15,10 @@ public class WalletService {
     FeignCheck feignCheck;
 
     // a method that calls remote service and returns the wallet
+    @CircuitBreaker(name = "getAccount", fallbackMethod = "getAccount2")
     public Wallet getAccountDetails(long accountNumber){
+
+        System.out.println("------Making Remote Call.........");
         String URL = "http://ACCOUNT-SERVICE/account/" + accountNumber;
         // get the Account object from the remote service
         //Account account = rest.getForObject(URL, Account.class);
@@ -22,5 +26,11 @@ public class WalletService {
         // Initialize wallet from the object
         Wallet wallet = new Wallet("MyPay", 500, 500+account.getBalance(), account);
         return wallet;
+    }
+
+    // fallback method that is automatically executed if the remote service is down
+    public Wallet getAccount2(long accountNumber, Throwable throwable){
+        System.out.println("________Fallback:method: Alternate response_________");
+        return new Wallet("MyPay", 500, 0, null);
     }
 }
