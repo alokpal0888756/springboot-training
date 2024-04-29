f367ef19f0046f6930bd5946fa8187a4661389c6
alok0888756 <alok0888756@gmail.com>
2024-04-29 01:06:17 -0400

    training completed

---
+++ test/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+abstract class A{
+    private int id;
+
+    public A() {
+    }
+
+    public A(int id) {
+        this.id = id;
+    }
+}
